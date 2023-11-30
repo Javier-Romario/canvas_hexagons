ee2afb610a5db961246ef67db283625918580f5c
Javier Harford <javierharford@Javiers-MacBook-Air.local>
2023-11-30 22:36:43 +0000

    add basic centered hexagon with gradient

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect, useRef } from 'react'
+import './App.css'
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+function App() {
+  const [count, setCount] = useState(0)
+  const ref = useRef(null)
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    const canvas = ref.current
+    const ctx = canvas.getContext('2d')
+    ctx.height = windowDimensions.height
+    ctx.width = windowDimensions.width
+
+
+    // ctx.save();
+    // const rectHeight = 100
+    // const rectWidth = 100
+    // ctx.rect(
+    //   (canvas.width / 2 - (rectWidth / 2)),
+    //   (canvas.height / 2 - (rectHeight / 2)),
+    //   rectWidth,
+    //   rectHeight
+    // );
+    // ctx.opacity = 0.8;
+    // ctx.fillStyle = 'salmon'
+    // ctx.fill();
+    // ctx.restore()
+    const x = (canvas.width / 2)
+    const y = (canvas.height / 2)
+
+    const r = 100;
+    const a = 2 * Math.PI / 6;
+    const grd = ctx.createLinearGradient(x, y, x + r, y + r);
+    grd.addColorStop(0, "#1e2e2e");
+    grd.addColorStop(1, "#f2c2e1");
+
+    for (let i = 0; i < 6; i++) {
+      const xval = x + r * Math.cos(a * i)
+      const yval = y + r * Math.sin(a * i)
+      ctx.lineTo(xval, yval);
+    }
+    ctx.fillStyle = grd
+    ctx.closePath();
+    ctx.fill();
+
+  }, [ref, windowDimensions])
+
+  return (
+    <main className="main">
+      <h1>hexagons</h1>
+      <canvas className="canvas" ref={ref} height={windowDimensions.height} width={windowDimensions.width} />
+    </main>
+  )
+}
+
+export default App
