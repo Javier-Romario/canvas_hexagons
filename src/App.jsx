99c05cb5c189987b33c5975cc365d7b7c8249371
Javier Harford <javierharford@Javiers-MacBook-Air.local>
2023-12-02 22:15:44 +0000

    refactor and remix a little

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,98 @@
-import { useState, useEffect, useRef } from 'react'
-import './App.css'
+import { useState, useEffect, useRef, useCallback } from "react";
+import { useControls } from "leva";
+
+import "./App.css";
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
   return {
     width,
-    height
+    height,
   };
 }
 function App() {
-  const ref = useRef(null)
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const ref = useRef(null);
+  const { size } = useControls({
+    size: {
+      value: 100,
+      min: 100,
+      max: 400,
+      step: 50,
+    },
+  });
+  const [context, setContext] = useState(null);
+
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions(),
+  );
   useEffect(() => {
     function handleResize() {
       setWindowDimensions(getWindowDimensions());
     }
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const drawHexagon = useCallback(
+    (ctx) => {
+      // console.log(ctx);
+
+      // clear the canvas
+      ctx.height = windowDimensions.height;
+      ctx.width = windowDimensions.width;
+
+      const x = ctx.width / 2;
+      const y = ctx.height / 2;
+
+      // const a = (2 * Math.PI) / 6;
+      const grd = ctx.createLinearGradient(
+        x,
+        y,
+        x + (size || 100),
+        y + (size || 100),
+      );
+      grd.addColorStop(0, "#1e2e2e");
+      grd.addColorStop(1, "#f2c2e1");
+
+      for (let side = 0; side <= 6; side++) {
+        ctx.lineTo(
+          x + size * Math.cos((side * 2 * Math.PI) / 6),
+          y + size * Math.sin((side * 2 * Math.PI) / 6),
+        );
+      }
+      ctx.fillStyle = grd;
+      ctx.closePath();
+      ctx.fill();
+      // console.log({ ctx });
+    },
+    [windowDimensions, size],
+  );
+
+  useEffect(() => {
+    // clear canvas
+    if (ref.current) {
+      const canvas = ref.current;
+      const ctx = canvas.getContext("2d");
+      setContext(ctx);
+    }
   }, []);
 
   useEffect(() => {
-    const canvas = ref.current
-    const ctx = canvas.getContext('2d')
-    ctx.height = windowDimensions.height
-    ctx.width = windowDimensions.width
-
-    const x = (canvas.width / 2)
-    const y = (canvas.height / 2)
-
-    const r = 100;
-    const a = 2 * Math.PI / 6;
-    const grd = ctx.createLinearGradient(x, y, x + r, y + r);
-    grd.addColorStop(0, "#1e2e2e");
-    grd.addColorStop(1, "#f2c2e1");
-
-    for (let i = 0; i < 6; i++) {
-      const xval = x + r * Math.cos(a * i)
-      const yval = y + r * Math.sin(a * i)
-      ctx.lineTo(xval, yval);
+    if (context) {
+      drawHexagon(context);
     }
-    ctx.fillStyle = grd
-    ctx.closePath();
-    ctx.fill();
-
-  }, [windowDimensions])
+  }, [drawHexagon, context]);
 
   return (
     <main className="main">
-      <canvas className="canvas" ref={ref} height={windowDimensions.height} width={windowDimensions.width} />
+      <canvas
+        className="canvas"
+        ref={ref}
+        height={windowDimensions.height}
+        width={windowDimensions.width}
+      />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
